46d66e414302e55d4525078cd5253947d7bd5743
ezhouqiyun <v_zhouqiyun@100tal.com>
2019-11-11 14:58:01 +0800

    添加人员弹框

--- src/components/modules/seniorexecutive/ModifyUser/index.vue
+++ src/components/modules/seniorexecutive/ModifyUser/index.vue
@@ -11,33 +11,33 @@
     </div>
     <el-form
       :rules="userRules"
-      label-width="140px"
+      label-width="110px"
       ref="userForm"
       :model="userForm"
       class="user-form"
     >
-      <el-form-item label="姓名/工号" prop="workcdoe">
-        <el-input v-model="userForm.workcdoe"></el-input>
+      <el-form-item label="姓名/工号:" prop="workcdoe">
+        <el-input v-model="userForm.workcdoe" @input="searchME"></el-input>
       </el-form-item>
-      <el-form-item label="邮箱" prop="email">
-        <span>{{ userForm.email }}</span>
+      <el-form-item label="邮箱:" prop="email">
+        <el-input v-model="userForm.email"></el-input>
       </el-form-item>
-      <el-form-item label="直接上级" prop="superior_workcode">
+      <el-form-item label="直接上级:" prop="superior_workcode">
         <el-input v-model="userForm.superior_workcode"></el-input>
       </el-form-item>
-      <el-form-item label="隔级" prop="isolation_workcode">
+      <el-form-item label="隔级:" prop="isolation_workcode">
         <el-input v-model="userForm.isolation_workcode"></el-input>
       </el-form-item>
-      <el-form-item label="总裁" prop="president_workcode">
+      <el-form-item label="总裁:" prop="president_workcode">
         <el-input v-model="userForm.president_workcode"></el-input>
       </el-form-item>
-      <el-form-item label="HRBP" prop="hrbp_workcode">
+      <el-form-item label="HRBP:" prop="hrbp_workcode">
         <el-input v-model="userForm.hrbp_workcode"></el-input>
       </el-form-item>
-      <el-form-item label="HRD" prop="hrd_workcode">
+      <el-form-item label="HRD:" prop="hrd_workcode">
         <el-input v-model="userForm.hrd_workcode"></el-input>
       </el-form-item>
-      <el-form-item label="高管类别" prop="executive_type">
+      <el-form-item label="高管类别:" prop="executive_type">
         <el-select
           v-model="userForm.executive_type"
           clearable
@@ -52,21 +52,26 @@
           </el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="上传指标">
+      <el-form-item label="上传指标:">
         <el-upload
           class="upload-demo"
-          :action="actionURL"
-          drag
+          action="actionURL"
           :uploadSuccess="uploadSuccess"
         >
-          选择文件
+          <el-button size="small" type="primary">选择文件</el-button>
         </el-upload>
       </el-form-item>
-      <el-form-item label="提示信息"> </el-form-item>
+      <el-form-item label="提示信息:">
+        <span class="tip-info">上传成功！共1人。</span>
+      </el-form-item>
     </el-form>
     <div slot="footer">
       <el-row type="flex" justify="center">
-        <el-button round size="medium" @click="submit" type="primary"
+        <el-button
+          round
+          size="medium"
+          @click="submit('userForm')"
+          type="primary"
           >确定</el-button
         >
         <el-button round size="medium" @click="close">取消</el-button>
@@ -76,7 +81,7 @@
 </template>
 <script>
 import {} from "@/constants/TEXT";
-import { postAddPerson } from "@/constants/API";
+import { postAddPerson, getUserDetail } from "@/constants/API";
 import { AsyncComp } from "@/utils/asyncCom";
 export default {
   props: {
@@ -113,18 +118,71 @@ export default {
         hrd_workcode: "",
         executive_type: ""
       },
-      userRules: {},
+      userRules: {
+        workcdoe: [
+          { required: true, message: "请输入姓名或工号", trigger: "blur" }
+        ],
+        email: [
+          { required: true, message: "请输入姓名或工号", trigger: "blur" }
+        ],
+        superior_workcode: [
+          { required: true, message: "请输入姓名或工号", trigger: "blur" }
+        ],
+        isolation_workcode: [
+          { required: true, message: "请输入姓名或工号", trigger: "blur" }
+        ],
+        president_workcode: [
+          { required: true, message: "请输入姓名或工号", trigger: "blur" }
+        ],
+        hrbp_workcode: [
+          { required: true, message: "请输入姓名或工号", trigger: "blur" }
+        ],
+        hrd_workcode: [
+          { required: true, message: "请输入姓名或工号", trigger: "blur" }
+        ],
+        executive_type: [
+          { required: true, message: "请输入姓名或工号", trigger: "change" }
+        ]
+      },
       actionURL: ""
     };
   },
   components: {},
   watch: {},
   methods: {
+    searchME(value) {
+      if (value != "") {
+        getUserDetail({
+          empID: value,
+          email: value
+        })
+          .then(res => {
+            if (res) {
+              this.userForm.email = res.email;
+            } else {
+              this.userForm.email = "";
+            }
+          })
+          .catch(e => {});
+      } else {
+        this.infoForm.email = "";
+      }
+    },
     close() {
       this.$emit("close");
     },
-    submit() {
-      // 确定按钮
+    submit(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          postAddPerson(this.performanceId, this.userForm)
+            .then(res => {
+              console.log(res);
+            })
+            .catch(e => {});
+        } else {
+          return false;
+        }
+      });
     },
     uploadSuccess(response, file, fileList) {
       if (
@@ -162,5 +220,14 @@ export default {
 <style scoped>
 .modify-user >>> .el-dialog__header {
   border-bottom: 1px solid #e4e7ed;
+}
+.modify-user >>> .el-form-item__content {
+  width: 294px;
+}
+.modify-user >>> .el-form-item__content .el-select {
+  width: 100%;
+}
+.modify-user .tip-info {
+  color: #eb0c00;
 }
 </style>
