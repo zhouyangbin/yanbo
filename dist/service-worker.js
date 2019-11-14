96ae3a08bd56bd9eef9d4df570a21d21ed680d45
ezhouqiyun <v_zhouqiyun@100tal.com>
2019-11-14 19:48:11 +0800

    打包部署到测试环境

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "//pingfen-executive.chengjiukehu.com/precache-manifest.23215349129424dbf34505faa33a04fa.js"
+  "//pingfen-executive.chengjiukehu.com/precache-manifest.0dc652dc46933293a54e44309654f1b8.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "pingfen-pc"});
