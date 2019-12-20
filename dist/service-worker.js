a1136dc57a72099b5520873ab3ab3e1271ce6b40
dreambo8563 <dreambo8563@outlook.com>
2019-12-20 15:50:30 +0800

    test:

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0bda8a002f9b7ceb436f0bfde7317827.js"
+  "/precache-manifest.7b44599e85eab7059cf0bf490f9b7e5d.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "pingfen-pc"});
